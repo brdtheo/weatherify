1ae6c9283ab108b98f01702be13e47ae7d15df48
Théo Billardey <theo.billardey@gmail.com>
2021-01-27 15:02:10 +0100

    add weather city & date, weather state

--- src/WeatherContent.svelte
+++ src/WeatherContent.svelte
@@ -4,6 +4,14 @@
   <div class="weather-website-name">the.weather</div>
   <div class="bottom-content">
     <div class="weather-temperature">16</div>
+    <div class="weather-city-date">
+      <h1>London</h1>
+      <span class="weather-date">06:09 - Monday, 9 Sep '19</span>
+    </div>
+    <div class="weather-state">
+      <span class="state-icon"><ion-icon name="partly-sunny-outline" /></span>
+      Cloudy
+    </div>
   </div>
 </div>
 
@@ -16,22 +24,53 @@
     padding: 50px 80px 100px;
     color: #fff;
   }
-  
+
   .weather-website-name {
     font-weight: 500;
     font-size: 15px;
-    font-family: 'Lato';
+    font-family: "Lato";
   }
 
   .bottom-content {
     line-height: 1;
+    display: flex;
+    align-items: flex-end;
   }
 
   .weather-temperature {
-    font-size: 100px;
+    font-size: 150px;
   }
 
   .weather-temperature::after {
-    content: '°'
+    content: "°";
+  }
+
+  .weather-city-date {
+    padding: 0 40px 0 10px;
+  }
+
+  .weather-city-date h1 {
+    font-weight: 400;
+    font-size: 80px;
+  }
+
+  .weather-state {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .weather-city-date,
+  .weather-state {
+    padding-bottom: 20px;
+  }
+
+  .state-icon {
+    font-size: 50px;
+  }
+
+  .weather-date,
+  .weather-state {
+    font-weight: 300;
+    font-family: "Lato", sans-serif;
   }
 </style>
