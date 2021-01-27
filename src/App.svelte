8d31fc812ba3f831e817ab82d72f73de44543d0b
Théo Billardey <theo.billardey@gmail.com>
2021-01-27 18:25:43 +0100

    set background position

--- src/App.svelte
+++ src/App.svelte
@@ -19,7 +19,7 @@
     background: url("https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1601&q=80");
     background-repeat: no-repeat;
     background-size: cover;
-    background-position: top;
+    background-position: 0 -150px;
   }
 
   .main-container-layout {
