79037c9d17183b00609e897c903791d8f643cf93
Théo Billardey <theo.billardey@gmail.com>
2021-01-27 10:26:58 +0100

    add Sidebar

---
+++ src/Sidebar.svelte
@@ -0,0 +1,12 @@
+<script></script>
+
+<div class="sidebar">sidebar</div>
+
+<style>
+  .sidebar {
+    backdrop-filter: blur(18px);
+    background: rgba(255,255,255,.05);
+    width: 420px;
+    box-shadow: 0 -5px 16px 10px rgba(0,0,0,.1);
+  }
+</style>
