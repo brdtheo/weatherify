71186c60a9b3a16d54182b76f3919b11ad67fb33
Théo Billardey <theo.billardey@gmail.com>
2021-01-27 10:28:53 +0100

    import WeatherContent, Sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,28 @@
-<script></script>
+<script>
+  import Sidebar from "./Sidebar.svelte";
+  import WeatherContent from "./WeatherContent.svelte";
+</script>
 
 <main>
-  <div class="main-container" />
+  <div class="main-container">
+    <div class="main-container-layout">
+      <WeatherContent />
+      <Sidebar />
+    </div>
+  </div>
 </main>
 
 <style>
   .main-container {
     width: 100%;
     height: 100%;
-    background: url("https://images.unsplash.com/photo-1463889253028-c66b99254e18?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80");
+    background: url("https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1601&q=80");
     background-repeat: no-repeat;
     background-size: cover;
+  }
+
+  .main-container-layout {
+    display: flex;
+    height: 100%;
   }
 </style>
