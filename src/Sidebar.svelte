b52632a254d3c893b51355b986fcd1051ff93e1b
Théo Billardey <theo.billardey@gmail.com>
2021-01-29 23:48:57 +0100

    fetch sidebar data

--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -1,18 +1,76 @@
-<script></script>
+<script>
+  $: input = "";
+  $: weather = {
+    cityName: "City",
+    temperature: null,
+    cloudy: null,
+    humidity: null,
+    windSpeed: null,
+  };
+
+  let promise = getWeather();
+
+  function pressEnter(key) {
+    if (key.keyCode === 13) {
+      getWeather();
+    }
+  }
+
+  function setCity(e) {
+    if (e.type === "click") {
+      input = e.target.innerText;
+      getWeather();
+    }
+  }
+
+  async function getWeather() {
+    const query = input;
+    if (query) {
+      const res = await fetch(
+        `http://api.weatherstack.com/current?access_key=bc78ee92848d943acdb1658e8acdc877&query=${query}&units=m`
+      );
+      if (res.ok) {
+        const json = await res.json();
+        weather.cloudy = json.current.cloudcover;
+        weather.humidity = json.current.humidity;
+        weather.windSpeed = json.current.wind_speed;
+        console.log(json);
+      }
+    }
+  }
+</script>
 
 <div class="sidebar">
   <section>
     <div class="sidebar-search">
-      <input type="text" placeholder="Another location" />
-      <button class="search-button" type="button" role="button">
+      <input
+        type="text"
+        placeholder="Another location"
+        bind:value={input}
+        on:keyup={pressEnter}
+      />
+      <button
+        class="search-button"
+        type="button"
+        role="button"
+        on:click={getWeather}
+      >
         <ion-icon name="search-outline" />
       </button>
     </div>
     <div class="sidebar-list">
-      <button class="sidebar-list-item">Birmingham</button>
-      <button class="sidebar-list-item">Manchester</button>
-      <button class="sidebar-list-item">New York</button>
-      <button class="sidebar-list-item">California</button>
+      <button type="button" class="sidebar-list-item" on:click={setCity}>
+        Birmingham
+      </button>
+      <button type="button" class="sidebar-list-item" on:click={setCity}>
+        Manchester
+      </button>
+      <button type="button" class="sidebar-list-item" on:click={setCity}>
+        New York
+      </button>
+      <button type="button" class="sidebar-list-item" on:click={setCity}>
+        California
+      </button>
     </div>
   </section>
   <section>
@@ -20,15 +78,15 @@
     <div class="sidebar-list">
       <div class="sidebar-list-item">
         <span>Cloudy</span>
-        <span>86%</span>
+        <span>{weather.cloudy}%</span>
       </div>
       <div class="sidebar-list-item">
         <span>Humidity</span>
-        <span>62%</span>
+        <span>{weather.humidity}%</span>
       </div>
       <div class="sidebar-list-item">
         <span>Wind</span>
-        <span>8km/h</span>
+        <span>{weather.windSpeed}km/h</span>
       </div>
     </div>
   </section>
@@ -105,6 +163,7 @@
     padding-bottom: 8px;
     font: inherit;
     color: inherit;
+    transition: border-bottom-color ease-in-out 100ms;
   }
 
   input:focus {
@@ -122,13 +181,14 @@
 
   .search-button {
     background: #d66d32;
+    color: #000;
     border: none;
     width: 80px;
     height: 80px;
     display: inline-flex;
     justify-content: center;
     align-items: center;
-    font-size: 30px;
+    font-size: 25px;
   }
 
   .search-button:focus {
@@ -155,7 +215,8 @@
     transition: color ease-in 100ms;
   }
 
-  button.sidebar-list-item:focus, button.sidebar-list-item:hover {
+  button.sidebar-list-item:focus,
+  button.sidebar-list-item:hover {
     color: #fff;
     outline: none;
   }
